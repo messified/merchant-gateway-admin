c5a65e2c6c80bed677cb14fc14df1b8967453385
messified <jesse.c.reese@gmail.com>
2019-07-30 18:41:22 -0700

    initial commit with extra goodies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './admin/shared/line-chart/line-chart.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
